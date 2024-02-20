205b2a3fee3f83d804f6c32efdc6f1ae862d0155
jischoi <60391224+coisu@users.noreply.github.com>
2024-02-20 00:49:36 +0000

    bring back entrypoint file

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,40 +1,97 @@
 #!/bin/sh
-echo "Starting entrypoint.sh script..."
 
-# Check if the database directory exists
 if [ ! -d /var/lib/mysql/$DB_NAME ]; then
-    mariadb-install-db --datadir=/var/lib/mysql --user=mysql
-    # Create directory for pidfile of running process
-    mkdir -p /run/mysqld
-    chown -R mysql:mysql /run/mysqld /var/lib/mysql
-    # Start MariaDB in safe mode in the background
-    /usr/bin/mysqld_safe --datadir='/var/lib/mysql' --user=mysql &
-    sleep 1
-	echo "Waiting for MariaDB service to start..."
-    until mysqladmin ping -h localhost >/dev/null 2>&1; do
-        sleep 1
-    done
 
-    echo "Initializing database..."
-
-    # Initialize the database and create users
-    mysql -h localhost -u root -p"$MYSQL_ROOT_PASSWORD" <<EOF
-CREATE DATABASE IF NOT EXISTS $DB_NAME CHARACTER SET utf8;
-CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
-GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$MYSQL_USER'@'%';
-FLUSH PRIVILEGES;
-ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
-DELETE FROM mysql.user WHERE User='';
-DROP DATABASE IF EXISTS test;
-DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
-FLUSH PRIVILEGES;
-EOF
-
-    # Shutdown MariaDB
-    mysqladmin -uroot --password=$MYSQL_ROOT_PASSWORD shutdown
+	mariadb-install-db --datadir=/var/lib/mysql --user=mysql
+	mkdir -p /run/mysqld    #pidfile of running process
+	chown -R mysql:mysql /run/mysqld /var/lib/mysql
+	/usr/bin/mysqld_safe --datadir='/var/lib/mysql' --user=mysql & #run mariadb insafemode in background
+	sleep 1
+	mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME CHARACTER SET utf8;CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$MYSQL_USER'@'%' WITH GRANT OPTION;FLUSH PRIVILEGES;ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';DELETE FROM mysql.user WHERE User='';DROP DATABASE IF EXISTS test;DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';FLUSH PRIVILEGES;"
+	mysqladmin -uroot --password=$MYSQL_ROOT_PASSWORD shutdown
 fi
 
-echo "Starting MariaDB..."
-
-# Start MariaDB
 /usr/bin/mysqld_safe --datadir='/var/lib/mysql'
+
+
+
+
+
+
+# #!/bin/sh
+
+# # Check if the database directory exists
+# if [ ! -d /var/lib/mysql/$DB_NAME ]; then
+#     mariadb-install-db --datadir=/var/lib/mysql --user=mysql
+#     # Create directory for pidfile of running process
+#     mkdir -p /run/mysqld
+#     chown -R mysql:mysql /run/mysqld /var/lib/mysql
+#     # Start MariaDB in safe mode in the background
+#     /usr/bin/mysqld_safe --datadir='/var/lib/mysql' --user=mysql &
+#     sleep 1
+# 	echo "Waiting for MariaDB service to start..."
+#     until mysqladmin ping -h localhost >/dev/null 2>&1; do
+#         sleep 1
+#     done
+
+#     # Initialize the database and create users
+#     mysql -h localhost -u root -p"$DB_ROOT_PW" <<EOF
+# CREATE DATABASE IF NOT EXISTS $DB_NAME CHARACTER SET utf8;
+# CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$MARIADB_PW';
+# GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';
+# FLUSH PRIVILEGES;
+# ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PW';
+# DELETE FROM mysql.user WHERE User='';
+# DROP DATABASE IF EXISTS test;
+# DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+# FLUSH PRIVILEGES;
+# EOF
+
+#     # Shutdown MariaDB
+#     mysqladmin -uroot --password=$DB_ROOT_PW shutdown
+# fi
+
+# # Start MariaDB
+# /usr/bin/mysqld_safe --datadir='/var/lib/mysql'
+
+
+# #!/bin/sh
+# echo "Starting entrypoint.sh script..."
+
+# # Check if the database directory exists
+# if [ ! -d /var/lib/mysql/$DB_NAME ]; then
+#     mariadb-install-db --datadir=/var/lib/mysql --user=mysql
+#     # Create directory for pidfile of running process
+#     mkdir -p /run/mysqld
+#     chown -R mysql:mysql /run/mysqld /var/lib/mysql
+#     # Start MariaDB in safe mode in the background
+#     /usr/bin/mysqld_safe --datadir='/var/lib/mysql' --user=mysql &
+#     sleep 1
+# 	echo "Waiting for MariaDB service to start..."
+#     until mysqladmin ping -h localhost >/dev/null 2>&1; do
+#         sleep 1
+#     done
+
+#     echo "Initializing database..."
+
+#     # Initialize the database and create users
+#     mysql -h localhost -u root -p"$MYSQL_ROOT_PASSWORD" <<EOF
+# CREATE DATABASE IF NOT EXISTS $DB_NAME CHARACTER SET utf8;
+# CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+# GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$MYSQL_USER'@'%';
+# FLUSH PRIVILEGES;
+# ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+# DELETE FROM mysql.user WHERE User='';
+# DROP DATABASE IF EXISTS test;
+# DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+# FLUSH PRIVILEGES;
+# EOF
+
+#     # Shutdown MariaDB
+#     mysqladmin -uroot --password=$MYSQL_ROOT_PASSWORD shutdown
+# fi
+
+# echo "Starting MariaDB..."
+
+# # Start MariaDB
+# /usr/bin/mysqld_safe --datadir='/var/lib/mysql'
