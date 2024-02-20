cbf09ab7779dc66c6dece7ce3ca3d62fad0d9f8e
jischoi <60391224+coisu@users.noreply.github.com>
2024-02-20 00:46:09 +0000

    d

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,25 +1,24 @@
 #!/bin/sh
 echo "Starting entrypoint.sh script..."
 
-export PATH=$PATH:/usr/sbin:/sbin:/usr/local/sbin
-
+# Check if the database directory exists
 if [ ! -d /var/lib/mysql/$DB_NAME ]; then
-    /usr/sbin/mariadb-install-db --datadir=/var/lib/mysql --user=mysql
+    mariadb-install-db --datadir=/var/lib/mysql --user=mysql
     # Create directory for pidfile of running process
     mkdir -p /run/mysqld
     chown -R mysql:mysql /run/mysqld /var/lib/mysql
     # Start MariaDB in safe mode in the background
     /usr/bin/mysqld_safe --datadir='/var/lib/mysql' --user=mysql &
     sleep 1
-    echo "Waiting for MariaDB service to start..."
-    until /usr/bin/mysqladmin ping -h localhost >/dev/null 2>&1; do
+	echo "Waiting for MariaDB service to start..."
+    until mysqladmin ping -h localhost >/dev/null 2>&1; do
         sleep 1
     done
 
     echo "Initializing database..."
 
     # Initialize the database and create users
-    /usr/bin/mysql -h localhost -u root -p"$MYSQL_ROOT_PASSWORD" <<EOF
+    mysql -h localhost -u root -p"$MYSQL_ROOT_PASSWORD" <<EOF
 CREATE DATABASE IF NOT EXISTS $DB_NAME CHARACTER SET utf8;
 CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
 GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$MYSQL_USER'@'%';
@@ -32,7 +31,7 @@ FLUSH PRIVILEGES;
 EOF
 
     # Shutdown MariaDB
-    /usr/bin/mysqladmin -uroot --password=$MYSQL_ROOT_PASSWORD shutdown
+    mysqladmin -uroot --password=$MYSQL_ROOT_PASSWORD shutdown
 fi
 
 echo "Starting MariaDB..."
