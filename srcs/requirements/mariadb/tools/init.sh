4fdd7f4d9884d8f7b2a288d1713f4342d30a8328
Jisu Choi <cscard1002@gmail.com>
2024-02-22 16:15:19 +0100

    mariadb is ready

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -20,32 +20,19 @@ if [ ! -d /var/lib/mysql/$DB_NAME ]; then
 
     echo "Initializing database..."
 
-#     # Initialize the database and create users
+    # Initialize the database and create users
 
-#     mysql -h localhost -u root -p "$MYSQL_ROOT_PASSWORD" <<EOF
-# ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
-# DROP DATABASE IF EXISTS test;
-# DROP USER ''@'localhost';
-# DROP USER ''@'$(hostname)';
-# CREATE DATABASE IF NOT EXISTS '$DB_NAME';
-# CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
-# GRANT ALL ON '$DB_NAME'.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
-# FLUSH PRIVILEGES;
-# EOF
-# instead of GRANT ALL PRIVILEGES ON '$DB_NAME'.* TO '$MYSQL_USER'@'%';
+    mysqladmin -u root password "$MYSQL_ROOT_PASSWORD"
 
+    mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS $DB_NAME CHARACTER SET utf8;"
+    mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+    mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$MYSQL_USER'@'%';"
+    mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
+    mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "DELETE FROM mysql.user WHERE User='';"
+    mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "DROP DATABASE IF EXISTS test;"
+    mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';"
+    mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
 
-    # mysql -h localhost -u root -p "$MYSQL_ROOT_PASSWORD" <<EOF
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME CHARACTER SET utf8;"
-mysql -u root -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$MYSQL_USER'@'%';"
-mysql -u root -e "FLUSH PRIVILEGES;"
-mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
-mysql -u root -e "DELETE FROM mysql.user WHERE User='';"
-mysql -u root -e "DROP DATABASE IF EXISTS test;"
-mysql -u root -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';"
-mysql -u root -e "FLUSH PRIVILEGES;"
-# EOF
 
     # Shutdown MariaDB
     rc-service mariadb stop
